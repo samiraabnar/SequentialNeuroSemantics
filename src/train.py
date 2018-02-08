42f4fd41e219d09e53ed3fd62598b04377b8eabc
Samira Abnar <samiraabnar@gmail.com>
2018-02-08 10:53:12 +0100

    conflict fixed

--- src/train.py
+++ src/train.py
@@ -17,11 +17,19 @@ def train(model, sess, sv, train_x, train_y, test_x, test_y, test_words, train_w
         start_index = 0;
         end_index = start_index + model.hparams.batch_size
         print("input_size",len(indexes))
+        if train_x.shape > 2:
         while end_index < len(indexes):
-            summary, _, mse = sess.run([model.summ_op,model.train_op, model.mean_squared_loss],
-                                    feed_dict={model.input_states_batch: train_x[:,indexes[start_index:end_index]],
-                                               model.output_states_batch: train_y[indexes[start_index:end_index]], model.batch_size: len(indexes[start_index:end_index]),
-                                               model.p_keep_input: .6, model.p_keep_hidden: 0.9})
+            if train_x.shape > 2:
+                summary, _, mse = sess.run([model.summ_op,model.train_op, model.mean_squared_loss],
+                                        feed_dict={model.input_states_batch: train_x[:,indexes[start_index:end_index]],
+                                                   model.output_states_batch: train_y[indexes[start_index:end_index]], model.batch_size: len(indexes[start_index:end_index]),
+                                                   model.p_keep_input: .6, model.p_keep_hidden: 0.9})
+            else:
+                summary, _, mse = sess.run([model.summ_op,model.train_op, model.mean_squared_loss],
+                                        feed_dict={model.input_states_batch: train_x[indexes[start_index:end_index]],
+                                                   model.output_states_batch: train_y[indexes[start_index:end_index]], model.batch_size: len(indexes[start_index:end_index]),
+                                                   model.p_keep_input: .6, model.p_keep_hidden: 0.9})
+
             print("mse %f" % mse)
             start_index = end_index
             end_index = start_index + model.hparams.batch_size
